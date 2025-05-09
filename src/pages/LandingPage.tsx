b573281592a714706595afa8dd1ce3de32d514be
Jadonamite <jadonamite@gmail.com>
2025-05-09 21:19:13 +0100

    Okay

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,20 +1,15 @@
 import { useEffect } from "react";
-// import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
-// import AnimationSection from "../components/AnimationSection";
 import FeatureSection from "../components/FeatureSection";
 import StatsSection from "../components/StatsSection";
 import FAQSection from "../components/FAQSection";
 import CallToAction from "../components/CallToAction";
-// import CryptoFlowDemo from "../components/move.tsx";
-// import CryptoToCashAnimation from "../components/firstanimation";
 import Footer from "../components/Footer";
 import BlockchainLogos from "../components/Blockchains";
 import Nav from "../components/NavbarReset";
 
 const LandingPage = () => {
    useEffect(() => {
-      // Update the page title
       document.title = "OpenCash | P2P Digital Asset Exchange";
    }, []);
 
@@ -26,13 +21,8 @@ const LandingPage = () => {
             <HeroSection />
             <BlockchainLogos />
             <FeatureSection />
-
-            {/* <AnimationSection /> */}
             <StatsSection />
             <FAQSection />
-            {/* <CryptoToCashAnimation /> */}
-            {/* <CryptoFlowDemo /> */}
-
             <CallToAction />
          </main>
          <Footer />
